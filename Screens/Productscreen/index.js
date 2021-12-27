63c1609aa547b6070c5a0f080178df00461df559
Sanket Bagad <alencolins@gmail.com>
2021-12-27 23:24:31 +0530

    Working on Quantity Selector Component

--- Screens/Productscreen/index.js
+++ Screens/Productscreen/index.js
@@ -3,14 +3,28 @@ import { View, Text } from 'react-native'
 import styles from './styles'
 import product from "../../data/products";
 import products from '../../data/products';
+import { Picker } from '@react-native-picker/picker';
 
 export default function ProductScreen() {
+    const [selectedValue, setSelectedValue] = React.useState(product.options ? product.options[0] : null);
+
     return (
         <View>
             <Text style={styles.title}>{product.title}</Text>
+           
+            <Picker selectedValue={selectedValue} onValueChange={(itemValue, itemIndex) => (
+                setSelectedValue(itemValue)
+            )}>
+                {product.options.map(option => (
+                    <Picker.Item label={option} value={option} />
+                ))}
+                
+            </Picker>
 
             <Text style={styles.price}>{product.price}</Text>
+
             <Text style={styles.description}>{product.description}</Text>
+            
         </View>
     )
 }
