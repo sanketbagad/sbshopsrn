101cfda025ae54932112865f67738a867c93535f
Sanket Bagad <alencolins@gmail.com>
2021-12-27 15:31:14 +0530

    Walking through errors one by one

--- Screens/Homescreen/index.js
+++ Screens/Homescreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import FontAweSome from 'react-native-vector-icons/FontAwesome'
 import ProductItem from '../../Components/ProductItem'
 
@@ -17,40 +17,5 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 3,
     },
-    root : {
-        flexDirection : 'row',
-        margin: 10,
-        borderWidth: 1,
-        borderColor: '#d1d1d1',
-        borderRadius: 10,
-        backgroundColor: '#fff',
-    },
-    image: {
-        width: 150,
-        height: 150,
-        resizeMode: 'contain',
-        flex:2,
-    },
-    rightContainor: {
-        padding: 10,
-        flex: 3,
     
-    },
-    title: {
-        fontSize: 16,
-    },
-    description: {},
-    rating: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginVertical: 5,
-    },
-    price: {
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    star: {
-        margin: 2,
-    }
-
 })
