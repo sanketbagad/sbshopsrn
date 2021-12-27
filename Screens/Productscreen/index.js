ae993e91b1756e6f78a84821c421c41ad7680b3f
Sanket Bagad <alencolins@gmail.com>
2021-12-27 19:00:23 +0530

    Working on Product Screen

---
+++ Screens/Productscreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ProductScreen() {
+    return (
+        <View>
+            <Text>Product Screen</Text>
+        </View>
+    )
+}
