d73627bbf8a65601ef64cdc509ffc29963665928
Sanket Bagad <alencolins@gmail.com>
2021-12-27 23:06:33 +0530

    Adding Some Libraries fpr styling purposes

--- Screens/Productscreen/index.js
+++ Screens/Productscreen/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import styles from './styles'
+import product from "../../data/products";
+import products from '../../data/products';
 
 export default function ProductScreen() {
     return (
         <View>
-            <Text>Product Screen</Text>
+            <Text style={styles.title}>{product.title}</Text>
+
+            <Text style={styles.price}>{product.price}</Text>
+            <Text style={styles.description}>{product.description}</Text>
         </View>
     )
 }
---
+++ Screens/Productscreen/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 20,
+    },
+    price: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    description: {},
+    
+})
+
+export default styles;
